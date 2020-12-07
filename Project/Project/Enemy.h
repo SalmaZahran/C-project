f05167eca26e7408675ca226708cefd6c708ac20
SalmaZahran <71960697+SalmaZahran@users.noreply.github.com>
2020-12-07 21:15:59 +0200

    Update Enemy.h

--- Project/Project/Enemy.h
+++ Project/Project/Enemy.h
@@ -6,17 +6,23 @@
 #include "EnemyControl.h"
 #include "EnemyHealth.h"
 #include "GameEnvironment.h"
-#include "Player.h"
+//#include "Player.h"
 
 using namespace std;
 
 class Enemy {
 private:
+	int strength;
+	string type;
+
 	EnemyControl enemyControl;
 	EnemyHealth enemyHealth;
 	GameEnvironment gameEnvironment;
-	Player player;
 public:
+	Enemy() {
+		strength = (rand() % 5) + 1; // to get  avalue between 0 to 4 since we are gna hold 4 enemies inside the arraylist fo enemies in the 
+	}
+
 	void moveRight() {
 		enemyControl.setX(enemyControl.getX() + 5);
 	}
@@ -26,8 +32,31 @@ public:
 	}
 
 	void reducePlayerHealth() {
-		//if the enemy collides with player
-		//method in the other class somewhere 
+		if (Enemy.pe == "fire") {
+			if (collision == true) {
+				return setPlayerHealth = getPlayerHealth - 40;
+			}
+			return 0;
+		}
+		if (attacker.type == "water") {
+			if (collision == true) {
+				return setPlayerHealth = getPlayerHealth - 30;
+			}
+			return 0;
+		}
+		if (attacker.type == "ground") {
+			if (collision == true) {
+				return setPlayerHealth = getPlayerHealth - 10;
+
+			}
+			return 0;
+		}
+		if (attacker.type == "rock") {
+			if (collision == true) {
+				return setPlayerHealth = getPlayerHealth - 20;
+			}
+			return 0;
+
 	}
 
 
